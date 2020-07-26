83989765a2cb7711eb837df865120005b00cad64
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-26 11:36:15 +0200

    Return string in spinal case

---
+++ regex.js
@@ -0,0 +1,10 @@
+// Return string in spinal case
+const changeToSpinalCase = (str) => {
+  return str
+    .split(/\s_|(?=[A-Z])/g)
+    .join('-')
+    .toLowerCase();
+};
+
+console.log(changeToSpinalCase("Let's_get_things_done"));
+console.log(changeToSpinalCase('NoWayJosè'));
