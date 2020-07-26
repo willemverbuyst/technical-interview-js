c74a6b43cfa657729491686ce2c9f809e59ebc6f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-26 10:50:16 +0200

    Create questions for strings

---
+++ strings.js
@@ -0,0 +1,20 @@
+// Split string
+const splitString = (str) => {
+  return str.split(' ');
+};
+
+console.log(splitString('Vanilla JavaScript'));
+
+// Split Characters
+const splitCharacters = (str) => {
+  return str.split('');
+};
+
+console.log(splitCharacters('Vanilla JavaScript'));
+
+// Reverse a string
+const reverseString = (str) => {
+  return str.split('').reverse().join('');
+};
+
+console.log(reverseString('Vanilla JavaScript'));
