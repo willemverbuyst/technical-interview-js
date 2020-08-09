66a0bbd55292f52ca2aeee4e432535a01a06aa21
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-09 17:28:47 +0200

    Test reverseNumber

--- numbers.js
+++ numbers.js
@@ -10,56 +10,38 @@ const numberFunctions = {
     }
     return total;
   },
+  // Reverse number - Traversy Media
+  reverseNumber: (num) =>
+    parseInt(num.toString().split('').reverse().join('')) * Math.sign(num),
 };
 
-/***************************************************/
+// /***************************************************/
+// // FizzBuzz - Traversy Media
+// const fizzBuzz = () => {
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//       console.log('FizzBuzz');
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz');
+//     } else if (i % 3 === 0) {
+//       console.log('Fizz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
 
-// console.log(sumAll([1, 5]));
-
-// returns
-// 15
-
-/***************************************************/
-// Reverse number - Traversy Media
-const reverseNumber = (num) =>
-  parseInt(num.toString().split('').reverse().join('')) * Math.sign(num);
-
-console.log(reverseNumber(3456));
-console.log(reverseNumber(123));
-console.log(reverseNumber(-123));
-
-// returns
-// 6543
-// 321
-// -321
-
-/***************************************************/
-// FizzBuzz - Traversy Media
-const fizzBuzz = () => {
-  for (let i = 1; i <= 100; i++) {
-    if (i % 15 === 0) {
-      console.log('FizzBuzz');
-    } else if (i % 5 === 0) {
-      console.log('Buzz');
-    } else if (i % 3 === 0) {
-      console.log('Fizz');
-    } else {
-      console.log(i);
-    }
-  }
-};
-
-fizzBuzz();
-// returns
-// 1
-// 2
-// Fizz
-// 4
-// Buzz
-// ...
-// 14
-// FizzBuzz
-// 16
-// ...
+// fizzBuzz();
+// // returns
+// // 1
+// // 2
+// // Fizz
+// // 4
+// // Buzz
+// // ...
+// // 14
+// // FizzBuzz
+// // 16
+// // ...
 
 module.exports = numberFunctions;
--- numbers.test.js
+++ numbers.test.js
@@ -7,3 +7,15 @@ test('sumAll([1, 5]) returns 15', () => {
 test('sumAll([1, 5]) returns 15', () => {
   expect(numberFunctions.sumAll([1, 5])).toBeGreaterThanOrEqual(6);
 });
+
+test('reverseNumber(3456) returns 6543', () => {
+  expect(numberFunctions.reverseNumber(3456)).toBe(6543);
+});
+
+test('reverseNumber(321) returns 123', () => {
+  expect(numberFunctions.reverseNumber(321)).toBe(123);
+});
+
+test('reverseNumber(-1233456) returns -6543321', () => {
+  expect(numberFunctions.reverseNumber(-1233456)).toBe(-6543321);
+});
