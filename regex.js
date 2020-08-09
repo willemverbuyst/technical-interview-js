650e5195b9c8b9b317be29b4cd8a06b01abaa01f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-09 17:09:31 +0200

    Test replaceWordInString

--- regex.js
+++ regex.js
@@ -5,37 +5,13 @@ const regexFunctions = {
       .split(/\s|_|(?=[A-Z])/g)
       .join('-')
       .toLowerCase(),
+  // // Replace word in string, preserve capital
+  replaceWordInString: (str, before, after) => {
+    const capitals = /[A-Z]/;
+    return capitals.test(before[0])
+      ? str.replace(before, after[0].toUpperCase() + after.slice(1))
+      : str.replace(before, after);
+  },
 };
-
-// /***************************************************/
-// // Replace word in string, preserve capital
-// const replaceWordInString = (str, before, after) => {
-//   const capitals = /[A-Z]/;
-//   return capitals.test(before[0])
-//     ? str.replace(before, after[0].toUpperCase() + after.slice(1))
-//     : str.replace(before, after);
-// };
-
-// console.log(
-//   replaceWordInString(
-//     'A quick brown fox jumped over the lazy dog',
-//     'jumped',
-//     'leaped'
-//   )
-// );
-
-// console.log(
-//   replaceWordInString(
-//     'Today I will learn regular expressions',
-//     'Today',
-//     'Tomorrow'
-//   )
-// );
-
-// // returns
-// // A quick brown fox leaped over the lazy dog
-// // Tomorrow I will learn regular expressions
-
-// /***************************************************/
 
 module.exports = regexFunctions;
--- regex.test.js
+++ regex.test.js
@@ -10,6 +10,22 @@ test('changeToSpinalCase("Let\'s_get_things_done") is defined', () => {
   );
 });
 
-test('changeToSpinalCase("NoWayJosè") is defined', () => {
-  expect(regexFunctions.changeToSpinalCase('NoWayJosè')).toBe('no-way-josè');
+test("replaceWordInString('A quick brown fox jumped over the lazy dog','jumped','leaped') returns 'A quick brown fox leaped over the lazy dog'", () => {
+  expect(
+    regexFunctions.replaceWordInString(
+      'A quick brown fox jumped over the lazy dog',
+      'jumped',
+      'leaped'
+    )
+  ).toBe('A quick brown fox leaped over the lazy dog');
+});
+
+test("replaceWordInString('Today I will learn regular expressions','Today','Tomorrow') returns 'Tomorrow I will learn regular expressions'", () => {
+  expect(
+    regexFunctions.replaceWordInString(
+      'Today I will learn regular expressions',
+      'Today',
+      'Tomorrow'
+    )
+  ).toBe('Tomorrow I will learn regular expressions');
 });
